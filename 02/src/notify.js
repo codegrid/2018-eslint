332a47f90f83f6dee33f51dcde291c21142b25d8
cyokodog <cyokodog@gmail.com>
2018-02-04 22:00:55 +0900

    第二回のデモ

---
+++ 02/src/notify.js
@@ -0,0 +1,9 @@
+var tmpl=_.template('<div class="<%- type %>"><%- msg %></div>');
+var notify = function(msg, type){
+     var html = tmpl({msg: msg, type:'info'});
+    console.log(html);
+    $('.info').html(html);
+}
+setTimeout(function() {
+    notify("Hello ESLint!", 'info');
+}, 500);
