f31e97a53b00f09c7ae177009da68f91271f0fb1
cyokodog <cyokodog@gmail.com>
2018-02-15 12:05:16 +0900

    不要なサンプルコードを除去

--- 02/src/notify.js
+++
@@ -1,9 +0,0 @@
-var tmpl=_.template('<div class="<%- type %>"><%- msg %></div>');
-var notify = function(msg, type){
-     var html = tmpl({msg: msg, type:'info'});
-    console.log(html);
-    $('.info').html(html);
-}
-setTimeout(function() {
-    notify("Hello ESLint!", 'info');
-}, 500);
