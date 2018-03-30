bab8e05177e80ed06d962b9d44f5dd7b279dd497
cyokodog <cyokodog@gmail.com>
2018-03-30 20:25:20 +0900

    補正

--- 04/use-config-cg2018/src/screenshot.js
+++ 04/use-config-cg2018/src/screenshot.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
-
-puppeteer.launch().then(async (browser) => {
+puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
   await page.goto('https://www.google.co.jp/search?q=eslint+plugin&oq=eslint+plugin');
   await page.screenshot({ path: 'screen_shot.png' });
